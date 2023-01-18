cef8945dbc5e9aa7eae708270464f8a8c15596a2
Tartarpaste <tordvg@hotmail.com>
2023-01-18 13:46:10 +0100

    feat: completed kata nr.4

--- JS/Katas/app.js
+++ JS/Katas/app.js
@@ -1,3 +1,29 @@
+const inputArray1 = document.getElementById("input-array");
+const inputArray2 = document.getElementById("input-array-2");
+const btnComputeArray1 = document.getElementById("btn-compute-array");
+const btnComputeArray2 = document.getElementById("btn-compute-array-2");
+const hArrayLength1 = document.getElementById("h7-array-length");
+const hArrayLength2 = document.getElementById("h7-array-length-2");
+
+function countBoomerangs(array) {
+  let amountOfBoomerangs = 0;
+  array = inputArray1.value;
+  for (let i = 0; i < array.length - 2; i++) {
+    if (array[i] === array[i + 2] && array[i] !== array[i + 1]) {
+      amountOfBoomerangs++;
+    }
+  }
+  hArrayLength1.innerText = amountOfBoomerangs;
+  return amountOfBoomerangs;
+}
+
+const arrayLenght = (array) => (array = inputArray2.value);
+let count = array.flat(Infinity).length;
+hArrayLength2.innerText = count;
+
+btnComputeArray2.addEventListener("click", arrayLenght);
+btnComputeArray1.addEventListener("click", countBoomerangs);
+/*
 const inputArray = document.getElementById("input-array");
 const btnComputeArray = document.getElementById("btn-compute-array");
 const hArrayLength = document.getElementById("h7-array-length");
@@ -11,6 +37,8 @@ function calculateArray() {
 const arrayLenght = (array) => array.flat(Infinity).length;
 
 btnComputeArray.addEventListener("click", calculateArray);
+*/
+
 /*
 function teamFinalScore(x) {
   let twoPointer = 2 * 3;
